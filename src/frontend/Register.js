b8d0a1fb59a161465060b359b7fd21a268cce7bc
Kumar Gaurav <kumar.gaurav7@globallogic.com>
2022-08-29 13:24:07 +0530

    registered

--- src/frontend/Register.js
+++ src/frontend/Register.js
@@ -1,117 +1,67 @@
-import { useState, useEffect } from "react";
-import { Button, Form } from 'react-bootstrap'
-import { Link } from "react-router-dom";
-// import "./App.css";
-
-function Register() {
-  const initialValues = { username: "", email: "", password: "" };
-  const [formValues, setFormValues] = useState(initialValues);
-  const [formErrors, setFormErrors] = useState({});
-  const [isSubmit, setIsSubmit] = useState(false);
-  //form validation
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-    setFormValues({ ...formValues, [name]: value });
-  };
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    setFormErrors(validate(formValues));
-    setIsSubmit(true);
-    fetch("http://localhost:8000/home", {
-      methpd: "POST",
-      headers: {
-        'Accept': 'application/json',
-        'Contact-Type': 'application/json'
-      },
-      body: JSON.stringify(formValues)
-    }).then(res => {
-      // console.warn("res",res)
-      res.json().then((resp) => {
-        console.warn("resp", resp);
-      })
-    })
-    // console.log(name,email,password)
-  };
-
-  useEffect(() => {
-    console.log(formErrors);
-    if (Object.keys(formErrors).length === 0 && isSubmit) {
-      console.log(formValues);
+import React, { useState } from 'react';
+import { Form, Button } from 'semantic-ui-react';
+import { useForm } from "react-hook-form";
+// import {Form, Button} from "react"
+import {useNavigate} from 'react-router-dom'
+import UserDashboard from './Dashboard/UserDashboard';
+ function SignUp() {
+    const navigate=useNavigate();
+    const { register, handleSubmit, formState: { errors } } = useForm();
+    const {isLoggedIn,setIsLoggedIn}=useState(false);
+    const onSubmit = (data) => {
+        navigate('/user/dashboard')
+        setIsLoggedIn(true)
     }
-  }, [formErrors]);
-  const validate = (values) => {
-    const errors = {};
-    const regex = /^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/i;
-    if (!values.username) {
-      errors.username = "Username is required!";
-    }
-    if (!values.email) {
-      errors.email = "Email is required!";
-    } else if (!regex.test(values.email)) {
-      errors.email = "This is not a valid email format!";
-    }
-    if (!values.password) {
-      errors.password = "Password is required";
-    } else if (values.password.length < 4) {
-      errors.password = "Password must be more than 4 characters";
-    } else if (values.password.length > 10) {
-      errors.password = "Password cannot exceed more than 10 characters";
-    }
-    return errors;
-  };//form validation
-  // console.log(name,email,password)
-
-  return (
-    <div className="container">
-      {Object.keys(formErrors).length === 0 && isSubmit ? (
-        <div>Signed in successfully</div>
-      ) : (
-        <pre></pre>//{JSON.stringify(formValues, undefined, 2)}
-      )}
-
-      <Form onSubmit={handleSubmit} style={{backgroundColor:"wheat", padding:"1rem", border:"2px solid black", borderRadius:"20px"}}>
-        <h1>Registration Form</h1>
-        <div className="ui divider"></div>
-        <div className="ui form">
-          <div className="field">
-            <Form.Label>Username</Form.Label>
-            <Form.Control
-              type="text"
-              name="username"
-              placeholder="Username"
-              value={formValues.username}
-              onChange={handleChange}
-            />
-          </div>
-          <p>{formErrors.username}</p>
-          <div className="field">
-            <Form.Label>Email</Form.Label>
-            <Form.Control
-              type="text"
-              name="email"
-              placeholder="Email"
-              value={formValues.email}
-              onChange={handleChange}
-            />
-          </div>
-          <p>{formErrors.email}</p>
-          <div className="field">
-            <Form.Label>Password</Form.Label>
-            <Form.Control
-              type="password"
-              name="password"
-              placeholder="Password"
-              value={formValues.password}
-              onChange={handleChange}
-            />
-          </div>
-          <p>{formErrors.password}</p>
-          <Button className="fluid ui button blue">Submit</Button>
+    return (
+        <div>
+            <h1>Please Register</h1>
+            <Form onSubmit={handleSubmit(onSubmit)}>
+                <Form.Field>
+                    <label>First Name</label>
+                    <input
+                        placeholder='First Name'
+                        type="text"
+                        {...register("firstName", { required: true, maxLength: 10 })}
+                    />
+                </Form.Field>
+                {errors.firstName && <p>Please check the First Name</p>}
+                <Form.Field>
+                    <label>Last Name</label>
+                    <input
+                        placeholder='Last Name'
+                        type="text"
+                        {...register("lastName", { required: true, maxLength: 10 })}
+                    />
+                </Form.Field>
+                {errors.lastName && <p>Please check the Last Name</p>}
+                <Form.Field>
+                    <label>Email</label>
+                    <input
+                        placeholder='Email'
+                        type="email"
+                        {...register("email",
+                            {
+                                required: true,
+                                pattern: /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+                            })}
+                    />
+                </Form.Field>
+                {errors.email && <p>Please check the Email</p>}
+                <Form.Field>
+                    <label>Password</label>
+                    <input
+                        placeholder='Password'
+                        type="password"
+                        {...register("password", {
+                            required: true,
+                            pattern: /^(?=.\d)(?=.[a-z])(?=.*[A-Z]).{6,15}$/
+                        })}
+                    />
+                </Form.Field>
+                {errors.password && <p>Please check the Password</p>}
+                <Button type='submit'>Submit</Button>
+            </Form>
         </div>
-      </Form>
-      <Link style={{ marginTop: "0.5rem" }} className="btn btn-danger" to='/'>Login</Link>
-    </div>
-  );
+    )
 }
-export default Register;
+export default SignUp
